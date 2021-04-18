f4f213e9c64211d9a5b8b1d2900259a621764c9c
oleg rodin <oleg.rodin@gridnine.com>
2021-04-18 15:59:24 +0300

    update Boolean scenario and examples

--- src/com/incretio/java/se11/class_boolean/Demo.java
+++ src/com/incretio/java/se11/class_boolean/Demo.java
@@ -6,13 +6,15 @@ public class Demo {
     public static void main(String[] args) {
 //        constructorOne();
 //        constructorTwo();
+//        implicitTypeConversion();
+//        staticFactoryByPrimitive();
+//        staticFactoryByString();
     }
 
     // deprecated constructor by boolean
     private static void constructorOne() {
         // example 1
         {
-            boolean type = true;
             boolean sourceValue = true;
             Boolean booleanObject = new Boolean(sourceValue);
             System.out.println("sourceValue = true. Actual value = " + booleanObject);
@@ -48,7 +50,7 @@ public class Demo {
         {
             boolean sourceValue = true;
             Boolean booleanObject = sourceValue;
-            System.out.println("sourceValue = false. Actual value = " + booleanObject);
+            System.out.println("sourceValue = true. Actual value = " + booleanObject);
         }
         // example 6
         {
@@ -56,5 +58,55 @@ public class Demo {
             Boolean booleanObject = sourceValue;
             System.out.println("sourceValue = false. Actual value = " + booleanObject);
         }
+        // example 7
+        {
+            Boolean booleanObject = true;
+            System.out.println("sourceValue = true. Actual value = " + booleanObject);
+        }
+        // example 8
+        {
+            Boolean booleanObject = false;
+            System.out.println("sourceValue = false. Actual value = " + booleanObject);
+        }
+    }
+
+    // method as static factory by primitive
+    private static void staticFactoryByPrimitive() {
+        // example 9
+        {
+            boolean sourceValue = true;
+            Boolean booleanObject = Boolean.valueOf(sourceValue);
+            System.out.println("sourceValue = true. Actual value = " + booleanObject);
+        }
+        // example 10
+        {
+            boolean sourceValue = false;
+            Boolean booleanObject = Boolean.valueOf(sourceValue);
+            System.out.println("sourceValue = false. Actual value = " + booleanObject);
+        }
+    }
+
+    // method as static factory by String
+    private static void staticFactoryByString() {
+        // example 11
+        {
+            String sourceValue = "true";
+            Boolean booleanObject = Boolean.valueOf(sourceValue);
+            System.out.println("sourceValue = \"true\". Actual value = " + booleanObject);
+        }
+        // example 12
+        {
+            String sourceValue = "false";
+            Boolean booleanObject = Boolean.valueOf(sourceValue);
+            System.out.println("sourceValue = \"false\". Actual value = " + booleanObject);
+        }
+    }
+
+    private static void m() {
+        boolean result = new Boolean(true) == new Boolean(false);
+        System.out.println(result);
+
+        boolean result2 = Boolean.valueOf(true) == Boolean.valueOf("true");
+        System.out.println(result2);
     }
 }
